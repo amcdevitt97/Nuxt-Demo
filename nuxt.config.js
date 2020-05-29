cf7a8e38e482e942f29afd242004edb915a0de71
ricky <rickylalwani@live.com>
2020-05-28 17:51:46 -0700

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Citizenship Portal',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Citizenship Portal'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  eslint: {
+    emitWarning: true
+  },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div
+    class="bg-gray-100 min-h-screen flex justify-center items-center text-center mx-auto"
+  >
+    <div class="flex flex-col items-center">
+      <div>
+        <svg
+          class="h-24 w-24"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <mask
+            id="mask0"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="25"
+            y="1"
+            width="15"
+            height="23"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M39.9265 23.9353L25.9229 23.9343L32.9237 1.2641L39.9265 23.9353Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask0)">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M25.7512 -7.62939e-06H40.0001V24.7404H25.7512V-7.62939e-06Z"
+              fill="black"
+            />
+          </g>
+          <mask
+            id="mask1"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="8"
+            y="0"
+            width="24"
+            height="15"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M31.2834 0L26.9552 14.0094L8.62207 0L31.2834 0Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask1)">
+            <rect x="7.63953" width="25.236" height="13.9955" fill="black" />
+          </g>
+          <mask
+            id="mask2"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="17"
+            width="19"
+            height="23"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0.596191 25.9037L11.9259 17.246L18.9296 39.9153L0.596191 25.9037Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask2)">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M-0.429321 15.8013H19.9999V40.7223H-0.429321V15.8013Z"
+              fill="#030100"
+            />
+          </g>
+          <mask
+            id="mask3"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="1"
+            width="19"
+            height="23"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.00274 1.21899L18.3314 9.87828L0 23.8902L7.00274 1.21899Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask3)">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M-0.429077 2.28882e-05H18.9701V25.9142H-0.429077V2.28882e-05Z"
+              fill="black"
+            />
+          </g>
+          <mask
+            id="mask4"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="16"
+            y="25"
+            width="24"
+            height="15"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M20.9788 39.9257L16.6523 25.9157L39.3122 25.9142L20.9788 39.9257Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask4)">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.7897 19.2325H42.9184V41.9864H12.7897V19.2325Z"
+              fill="#2E2E2E"
+            />
+          </g>
+        </svg>
+      </div>
+      <h1 class="font-bold text-4xl mt-2">Citizenship Portal Prototype</h1>
+      <h2 class="text-2xl">A volunteer project by Humanity Forward</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>

7467d3fc6d7f799bf110ffb12a8377c7f2369974
Rodney <rtm36@zips.uakron.edu>
2020-05-30 19:01:46 -0400

    add styling

--- components/nav/DashboardHeader.vue
+++ components/nav/DashboardHeader.vue
@@ -1,17 +1,16 @@
 <template>
   <header role="banner" class="banner">
     <div class="masthead grid-col-2">
-      <div>
-        {{ 'Insert Icon and Website Name here Use are' }}
-        <!-- <logo></logo>
-        <p>Citizenship Portal</p>-->
+      <div class="title-container">
+        <!-- <logo /> -->
+        <p class="website-portal-title">Citizenship Portal</p>
       </div>
       <div>
         {{ 'User icon and dropdown here ' }}
         <!-- <user-dropdown></user-dropdown> -->
       </div>
     </div>
-    <nav class="navbar grid-col-auto-fill" role="navigation">
+    <nav class="navbar grid-col-auto-fill dropdown-container" role="navigation">
       <li v-for="i in 5" :key="i">Dropdown {{ i }}</li>
 
       <!-- <search-bar></search-bar> -->
@@ -28,4 +27,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.banner {
+  padding: 2em 2em 0 2em;
+}
+.website-portal-title {
+  font-size: 2em
+}
+.dropdown-container {
+  padding: space-between;
+}
+li {
+  display: inline-block;
+  text-align: center;
+  text-justify: auto;
+  color: #565C65;
+  padding-top: 1em;
+  padding-bottom: 2em;
+}
+
+li:hover {
+  background-color: #162D51
+}
+</style>
